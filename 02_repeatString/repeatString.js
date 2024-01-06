473ec1efb80c21d6b988ad8c9bdad3e1b61e9eb3
Tanat <55195958+tanat8350@users.noreply.github.com>
2024-01-07 02:58:09 +0700

    02 Answered

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  /* Solution 1 */
+  // if (num < 0) return "ERROR";
+  // return string.repeat(num);
 
+  /* Solution 2 */
+  let result = "";
+  if (num < 0) return "ERROR";
+  while (num > 0) {
+    result += string;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line

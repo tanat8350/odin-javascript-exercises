9489dfd0f422ce6d3c03a498a4e40e448b909fc1
Tanat <55195958+tanat8350@users.noreply.github.com>
2024-01-07 14:37:16 +0700

    05 Answered

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  let resultSum = 0;
+  let [start, end] = [num1, num2].sort((a, b) => a - b);
+  for (; start <= end; start++) {
+    resultSum += start;
+  }
+  return resultSum;
 };
 
 // Do not edit below this line

8ad69bdd7f7f1450a3f6374998c4c4564d52c647
Tanat <55195958+tanat8350@users.noreply.github.com>
2024-01-07 13:44:38 +0700

    04 Answered

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  args.forEach((agr) => {
+    const index = array.indexOf(agr);
+    if (index === -1) return;
+    array.splice(index, 1);
+  });
+  return array;
 };
 
 // Do not edit below this line
